3ecb17d078339424fcd9725f636c35747a4d2216
Antonin Duroy <antonin.duroy@gmail.com>
2015-10-25 11:50:38 +0100

    Ensure placeholder is unique

--- src/spin.py
+++ src/spin.py
@@ -59,7 +59,7 @@ class Spin():
             perforations = a.group(1).split(delimiter)
             parent = SpinTree(or_=True)
             placeholder = '__NODE__'+str(random.randint(0, 10000))
-            while placeholder not in self.placeholders:
+            while placeholder in self.placeholders:
                 placeholder = '__NODE__'+str(random.randint(0, 10000))
             insert_index = -1
             for perfo in perforations:
